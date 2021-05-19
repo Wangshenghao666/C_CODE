7fb26c676e35fa1cf3a33db5ffe4877f1f2b82d0
王生豪 <15340836075@163.com>
2021-05-19 13:45:55 +0800

    提交代码

---
+++ test_5_13/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("wang sheng hao\n");
+	return 0;
+}
---
+++ test_5_15/test_5_15/test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	//char ch = 'A';
+	//
+	//
+	int age = 20;
+		printf("%d\n", age);
+	//printf("%c\n",age);
+	return 0;
+}
