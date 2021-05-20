454c61d73a83f7c88d00b1ccd55023a3e54805db
王生豪 <15340836075@163.com>
2021-05-21 00:31:12 +0800

    提交两个函数相加和
    数组

--- test.c
+++ test.c
@@ -1,18 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//20000行代码   有效的代码,循环语句
-#include<stdio.h>
-#include<string.h>
+//两个函数相加
+//	sum = Add(num1, num2);
+//	sum = Add(a, b);
+//	printf("sum=%d\n", sum);
+//	re#include<stdio.h>
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int sum = 0;
+//	int a = 100;
+//	int b = 200;
+//turn 0;
+//}
 
+//这边是分界线
+//如果打印一组数组0--10
+#include<stdio.h>
 int main()
 {
-	int line = 0;
-	printf("加入比特\n");
-	while (line < 20000)
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int i = 0;
+	while (i < 10)
 	{
-		printf("敲一行代码：%d\n" ,line);
-		line++;
+		printf("%d\n", arr[i]);
+		i++;
 	}
-	if (line >= 20000)
-		printf("好offer\n");
 	return 0;
 }
