22e75f2cf055f79c0345450ef5df5571e6065372
王生豪 <2877959358@qq.com>
2021-07-30 21:49:26 +0800

    依次输入3个整数，结果从大到小。100以内3的倍数

---
+++ test_7_30/test_7_30/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	int z = 0;
+//	int max = 0;
+//	int min = 0;
+//	scanf("%d%d%d", &x, &y, &z);
+//	if (x > y)
+//	{
+//		max = x;
+//		min = y;
+//	}
+//	if (z > max)
+//		max = z;
+//	if (z < min)
+//		min = z;
+//	y = x + y + z - max - min;
+//	x = max;
+//	z = min;
+//	printf("从大到小排序：%d %d %d\n", x, y, z);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 3;
+//	while (i <= 100)
+//	{
+//		printf("%d\n", i);
+//		i += 3;
+//	}
+//	return 0;
+//}
+
+
